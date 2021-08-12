9ec0989a9925b8c8daf91685e2605f0b3cbb6741
Quan Nguyen <minhquan0902@gmail.com>
2021-08-12 18:58:45 +0800

    Saving user data to db

--- src/bin/InsulinGlucagonPump.java
+++ src/bin/InsulinGlucagonPump.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import javax.swing.*;
 
 
@@ -37,12 +39,12 @@ public class InsulinGlucagonPump extends JFrame {
 
 
         Automatic = new JButton("Start the Simulator");
-
+        // Info Panel
         JLabel info = new JLabel("Taking in your credentials help us save your data for future tracing");
-        info.setBounds(10,50, 100, 180);
+        info.setBounds(10,50, 50, 180);
         add(info);
         // Login panel
-        JLabel userLabel = new JLabel("Please Input your name: ");
+        JLabel userLabel = new JLabel("Your full name: ");
         userLabel.setBounds(10,20,80,25);
         add(userLabel);
         JTextField usernameText = new JTextField(20);
@@ -51,7 +53,7 @@ public class InsulinGlucagonPump extends JFrame {
         setVisible(true);
 
         // Login panel
-        JLabel emailLable = new JLabel("Please Input your email: ");
+        JLabel emailLable = new JLabel("Your Email add: ");
         emailLable.setBounds(10,20,80,25);
         add(emailLable);
         JTextField emailText = new JTextField(20);
@@ -59,10 +61,42 @@ public class InsulinGlucagonPump extends JFrame {
         add(emailText);
         setVisible(true);
 
+        // Weight panel
+        JLabel weightLabel = new JLabel("Please Input your Weight (in kg): ");
+        weightLabel.setBounds(10,20,80,25);
+        add(weightLabel);
+        JTextField weightText = new JTextField(20);
+        weightText.setBounds(100,20,165,25);
+        add(weightText);
+        setVisible(true);
+
+        // Height panel
+        JLabel heightLabel = new JLabel("Please Input your Height (in m): ");
+        heightLabel.setBounds(10,20,80,25);
+        add(heightLabel);
+        JTextField heightText = new JTextField(20);
+        heightText.setBounds(100,20,165,25);
+        add(heightText);
+        setVisible(true);
+
+        // Age input
+        JLabel Age = new JLabel("Please Input your Age (in number): ");
+        Age.setBounds(20,20,100,25);
+        add(Age);
+        JTextField AgeNumber = new JTextField(20);
+        AgeNumber.setBounds(120,20,200,25);
+        add(AgeNumber);
+        setVisible(true);
+
+        // Thanks Panel
+        JLabel thankYou = new JLabel("Thank you very much for your corporation :)");
+        info.setBounds(10,50, 50, 180);
+        add(thankYou);
 
 
 
-        
+
+
 
         add(Automatic);
       
@@ -82,15 +116,17 @@ public class InsulinGlucagonPump extends JFrame {
                 new GUI("Auto");
                 String username = usernameText.getText();
                 String email = emailText.getText();
+                String age = AgeNumber.getText();
+                String weight = weightText.getText();
+                String height = heightText.getText();
 
                 // Connect and store user data to MYSQL Database
-                getConnection(username, email);
+                getConnection(username, email, age, weight, height);
 
                 }     
         });
 
 
-        
       
         
    }
@@ -106,7 +142,7 @@ public class InsulinGlucagonPump extends JFrame {
 
     }
 
-    public static Connection getConnection(String username, String email )  {
+    public static Connection getConnection(String username, String email, String age, String weight, String height )  {
 
         try{
             String driver = "com.mysql.cj.jdbc.Driver";
@@ -115,10 +151,22 @@ public class InsulinGlucagonPump extends JFrame {
             String dbPassword = "mysql"; // if running localhost, provide your mysql password here
             Class.forName(driver);
             Connection conn = DriverManager.getConnection(url, dbUsername, dbPassword);
-            System.out.println("Connected");
+            System.out.println("Connected to the database successfully");
+            String sql = "INSERT INTO persons (username,email,age,weight,height) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, email);
+            statement.setString(3,age);
+            statement.setString(4, weight);
+            statement.setString(5, height);
+
+            int rows = statement.executeUpdate();
+            if (rows >0) {
+                System.out.println("Successfully Insert Data to database");
+            }
+            statement.close();
 
 
-            return conn;
 
         }catch(Exception e){
             System.out.println("Oops, error!");
